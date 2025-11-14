83b897eb745b2151657aa8abdf33b12e4b617e79
David Grace <davidgrace00@gmail.com>
2025-11-14 14:24:36 +0000

    Added EF Core

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,7 +1,11 @@
+using ToDo.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddToDoDbContext(
+    builder.Configuration
+);
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
---
+++ Infrastructure/ConfigureServices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDo.Infrastructure.Data;
+
+namespace ToDo.Infrastructure;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddToDoDbContext(
+        this IServiceCollection services,
+        IConfiguration configuration
+        )
+    {
+        services.AddDbContext<ToDoDbContext>(options =>
+        {
+            options.UseSqlServer(
+                configuration.GetConnectionString("ToDoDbContext")
+                );
+        });
+
+        return services;
+    }
+}
